7dcaaf44860f5de9b31831224b9833d36d8c04d8
NotABot <135363764+BryanTheLai@users.noreply.github.com>
2024-11-09 17:22:05 +0800

    Redirect "/" to "/home"

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { redirect } from "next/navigation";
 
 export default async function Index() {
+  redirect('/home');
   return (
     <>
       <Hero />

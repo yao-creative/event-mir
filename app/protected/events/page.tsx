551cc8ed67586988a2456405ea60c8d0db0caa0f
NotABot <135363764+BryanTheLai@users.noreply.github.com>
2024-11-15 14:35:28 +0800

    suspense wrapper

--- app/protected/events/page.tsx
+++ app/protected/events/page.tsx
@@ -1,11 +1,14 @@
 import Search from "@/components/ui/search";
 import React from "react";
-
+import { Suspense } from "react";
 export default function Page() {
   return (
     <div>
       <div>
-        <Search placeholder="search"></Search>
+        <Suspense>
+          {" "}
+          <Search placeholder="search"></Search>
+        </Suspense>
       </div>
     </div>
   );
